f1fb6c43f6a1f65e81b3b6d088a0f71fa0322b1b
reckbo <reckbo@bwh.harvard.edu>
2017-06-23 20:16:54 +0000

    ENH: make update.py work

--- db/update.py
+++ db/update.py
@@ -9,15 +9,16 @@ class App(cli.Application):
     runFind = cli.SwitchAttr(['--find'], help='Crawl project directories as well')
 
     def main(self):
-        dirs = local.cwd.dirs()
+        dirs = [p for p in local.cwd.list() if p.is_dir() and not p.startswith('.')]
         for dir in dirs:
-            projdir = '/' / dir.dirname.replace('---', '/')
+            projdir = local.path('/') / dir.name.replace('---', '/')
+            print("Updating '{}'".format(projdir))
             with local.cwd(projdir):
                 if self.runFind:
                     pnlproj('find')
-                pnlproj('diff')
+                pnlproj('status')
                 pnlproj('push')
 
 
 if __name__ == '__main__':
-    Diff.run()
+    App.run()
