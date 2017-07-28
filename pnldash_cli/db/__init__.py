69ab039768367b15d69b7d32f046a0a60c1a9cfd
reckbo <reckbo@bwh.harvard.edu>
2017-07-28 19:23:10 +0000

    STY: make db list print in table format

--- pnldash_cli/db/__init__.py
+++ pnldash_cli/db/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import logging
 log = logging.getLogger(__name__)
 from plumbum import cli, local, FG
@@ -6,8 +7,8 @@ from pnldash_config import *
 from pnldash_lib import open_db, make_extra
 import yaml
 
-
 PNLDASH_FILES = [PROJECT_YML, PATHS_CSV, PARAMS_CSV, EXTRA_CSV, DU_CSV]
+
 
 class Push(cli.Application):
     """Adds your project to the central database."""
@@ -41,8 +42,10 @@ class Report(cli.Application):
 class Open(cli.Application):
     """Open HTML dashboard in your browser (makes the dashboard first if it doesn't exist)."""
 
-    refresh = cli.Flag(['-r', '--refresh'], default=False,
-                       help="Remake the dashboard if it already exists.")
+    refresh = cli.Flag(
+        ['-r', '--refresh'],
+        default=False,
+        help="Remake the dashboard if it already exists.")
 
     def main(self):
 
@@ -51,21 +54,61 @@ class Open(cli.Application):
         gui_open('pnldashboard.html')
 
 
+def formatTable(ds, header=None):
+    """ Pretty print a list of dictionaries (myDict) as a dynamically sized table.
+   Original from http://stackoverflow.com/questions/17330139/python-printing-a-dictionary-as-a-horizontal-table-with-headers
+   """
+    myDict = ds
+    colList = header
+    if not colList: colList = sorted(list(myDict[0].keys()) if myDict else [])
+    myList = [colList]  # 1st row = header
+    for item in myDict:
+        myList.append([str(item[col] or '') for col in colList])
+    colSize = [max(map(len, col)) for col in zip(*myList)]
+    formatStr = ' | '.join(["{{:<{}}}".format(i) for i in colSize])
+    myList.insert(1, ['-' * i for i in colSize])  # Separating line
+    result = ''
+    for item in myList:
+        result = result + (formatStr.format(*item)) + '\n'
+    return result
+
+def printTable(ds, header=None):
+    print(formatTable(ds, header))
+
 class List(cli.Application):
     """List the projects in the central database"""
+
+    useCache = cli.Flag(['-c', '--cache'], default=False,
+                        help="Use cached result.")
 
     def main(self):
         PROJECT_YML_FILENAME = PROJECT_YML.name
 
+        #TODO refresh cache after a certain time
+        cache = local.path(__file__).dirname / '.dblist.yml'
+        if self.useCache and cache.exists():
+            with open(cache, 'r') as f:
+                print(f.read())
+                return
+
+        result = []
         with open_db() as (url, machine, dbpath):
             ymlfiles = [y for y in dbpath // ('*/' + PROJECT_YML_FILENAME)]
             for ymlfile in ymlfiles:
-                projdir = ymlfile.dirname.replace('---', '/')
+                projdir = '/' + ymlfile.dirname.name.replace('---', '/')
                 ymlstr = machine['cat'](ymlfile)
                 yml = yaml.load(ymlstr, Loader=yaml.loader.BaseLoader)
                 numpipelines = len(yml['pipelines'])
-                print(yml['name'] + " ({}) ({} pipeline(s))".format(projdir,
-                                                                numpipelines))
+                row = {'name': yml['name'],
+                       'directory': projdir,
+                       'num. pipelines': numpipelines
+                       }
+                result.append(row)
+
+        result = formatTable(result, header=['name', 'directory', 'num. pipelines'])
+        with open(cache, 'w') as f:
+            f.write(result)
+        print(result)
 
 
 class Info(cli.Application):
